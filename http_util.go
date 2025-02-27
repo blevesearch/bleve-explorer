8b2998810d1e48aab0bf0b6872f42dec85bee5d2
Christian González Di Antonio <christiangda@gmail.com>
2025-02-27 11:16:17 +0100

    fix: remove unused functions

--- http_util.go
+++ http_util.go
@@ -13,9 +13,6 @@ package main
 //go:generate go fmt .
 
 import (
-	"encoding/json"
-	"io"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -76,22 +73,4 @@ func docIDLookup(req *http.Request) string {
 
 func indexNameLookup(req *http.Request) string {
 	return muxVariableLookup(req, "indexName")
-}
-
-func showError(w http.ResponseWriter, r *http.Request,
-	msg string, code int) {
-	log.Printf("Reporting error %v/%v", code, msg)
-	http.Error(w, msg, code)
-}
-
-func mustEncode(w io.Writer, i interface{}) {
-	if headered, ok := w.(http.ResponseWriter); ok {
-		headered.Header().Set("Cache-Control", "no-cache")
-		headered.Header().Set("Content-type", "application/json")
-	}
-
-	e := json.NewEncoder(w)
-	if err := e.Encode(i); err != nil {
-		panic(err)
-	}
 }
